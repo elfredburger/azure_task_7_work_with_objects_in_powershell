37b606daf15ed0e0daa481c383ad29abd21202be
elfredburger <elfredfredburger@gmail.com>
2024-08-22 13:26:06 +0200

    solution

--- task.ps1
+++ task.ps1
@@ -1,39 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Write your code here
-=======
 
-# Create an empty list to store the regions
-$regionsWithVMSize = @()
-
-# Get all JSON files in the data directory
-$jsonFiles = Get-ChildItem -Path "'\.data'"
-
-# Loop through each JSON file
-foreach ($file in $jsonFiles) {
-    # Read the file content and convert JSON to PowerShell objects
-    $jsonData = Get-Content -Path $file.FullName | ConvertFrom-Json
-
-    # Search for the VM size 'Standard_B2pts_v2' in the JSON data
-    $foundVMSize = $jsonData | Where-Object { $_.name -eq "Standard_B2pts_v2" }
-
-    if ($foundVMSize) {
-        # Get the region name from the file name (removing the .json suffix)
-        $regionName = $file.Name.Replace('.json', '')
-        $regionsWithVMSize += $regionName
-    }
-}
-
-# Export the result list as JSON to the output file
-$regionsWithVMSize | ConvertTo-Json -Depth 10 | Set-Content -Path ".\result.json"
-
-
-
-
-
-
-=======
->>>>>>> 261cb5d (Solution)
 $regionsWithVMSize = @()
 
 $regions = Get-ChildItem -Path ".\data"
@@ -47,4 +12,3 @@ foreach ($region in $regions) {
 }
 
 $regionsWithVMSize | ConvertTo-Json -Depth 10 | Set-Content -Path ".\result.json"
->>>>>>> 6fffbb2 (Solution)
